0272547daf2faa431ece52342e49b722c95b1e6d
kushal1997 <kushalrao103@gmail.com>
2023-03-21 22:09:34 +0530

    learned how to use setCount

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [count,setCount]= useState(0);
+
+  function handleAdd(){
+    setCount(count+1);
+  }
+
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <div className="box">
+        <p>{count}</p>
+        <button onClick={handleAdd} className="add">ADD</button>
+        <button onClick={()=>setCount(count-1)} className="sub">SUB</button>
+        <button onClick={()=>setCount(0)} className="reset">Reset</button>
+      </div>
     </div>
   );
 }
