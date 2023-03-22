faf7b10ce7610cd867b7e63f575cfc0690e3ed55
kushal1997 <kushalrao103@gmail.com>
2023-03-22 09:28:15 +0530

    learned state state values

--- src/App.js
+++ src/App.js
@@ -31,12 +31,42 @@ import './App.css';
 // useState: Array State Value
 // ================================
 
+// function App(){
+//   const [tasks,setTasks]=useState([
+//         {id: 1, name:"Record Videos", completed:false },
+//         {id: 2, name:"Listem Music", completed:true},
+//         {id: 3, name:"Study", completed:false}
+//       ]);
+//   return (
+//     <div className="App">
+//         <h1>Task List</h1>
+//         <ul>
+//           {tasks.map((task) =>(
+//             <li key={task.id}>
+//               <span>{task.id} - {task.name}</span>
+//               <button>Delete</button>
+//             </li>
+//           ))}
+//         </ul>
+//     </div>
+//   );
+// }
+// export default App;
+
+
+// ================================
+// useState: Array setState Value
+// ================================
 function App(){
   const [tasks,setTasks]=useState([
         {id: 1, name:"Record Videos", completed:false },
         {id: 2, name:"Listem Music", completed:true},
         {id: 3, name:"Study", completed:false}
       ]);
+  function handleDelete(id){
+    setTasks(tasks.filter(task => id!== task.id));
+
+  }
   return (
     <div className="App">
         <h1>Task List</h1>
@@ -44,7 +74,7 @@ function App(){
           {tasks.map((task) =>(
             <li key={task.id}>
               <span>{task.id} - {task.name}</span>
-              <button>Delete</button>
+              <button onClick={()=> handleDelete(task.id)}>Delete</button>
             </li>
           ))}
         </ul>
@@ -52,7 +82,6 @@ function App(){
   );
 }
 export default App;
-
 
 
 
